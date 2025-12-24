ba92c4d9962fe55ff87932ec299207ab46a78a66
baramee <baramee.papim@gmail.com>
2025-12-24 15:10:21 +0700

    feat: Implement arithmetic operations in calculate function, enhance sumEvenNumbers to filter and sum even numbers, define Order type and totalByStatusWithDiscount function for order calculations, update isAdult function to handle undefined age, and add type checking in processData function.

--- src/ex-4.1.ts
+++ src/ex-4.1.ts
@@ -5,10 +5,11 @@ type User = {
 };
 
 function isAdult(user: User): boolean {
-  return user.age >= 18;
+  return (user.age ?? 0) >= 18;
 }
 
-const result = isAdult({ id: "u01", name: "John" });
+const result = isAdult({ id: "u01", name: "John"});
 console.log(result); // ควรได้ false
 
-// Error ที่เจอคือ
+// Error ที่เจอคือ ตอน excecute function isAdult เนื่องจากไม่มี argument ของ user.ageส่งมาจึงได้ output เป็น undefined เมื่อ return ออกมาจึงทำให้ผิด logic (undefined >= 18)
+//ดังนั้นจึงแก้ไขด้วยการกำหนด Nullish Coalescing Operator ให้กับ user.age เป็น syntax (user.age ?? 0) ความหมายคือ ถ้า user.age เป็น null หรือ undefined ให้ใช้ค่า 0 แทน
--- src/ex-4.2.ts
+++ src/ex-4.2.ts
@@ -1,8 +1,14 @@
 function processData(data: unknown): string {
-  return data.toUpperCase();
+  if (typeof data === "string") {
+    return data.toUpperCase();
+  } else {
+    return "Not a string"
+  }
+  
 }
 
 console.log(processData("hello")); // ควรได้ "HELLO"
 console.log(processData(123)); // ควรได้ "Not a string"
 
-// Error ที่เจอคือ
+// Error ที่เจอคือ ส่ง value ที่เป็น number เข้าไปใน function ที่ถูกกำหนดว่าต้อง return ผลลัพธ์ออกมาเป็น string 
+//วิธีแก้ไขคือ สร้าง if-else ขึ้นมา
